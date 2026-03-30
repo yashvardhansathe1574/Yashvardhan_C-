8567923b1170a4b1cc0a3ed2c258b0a396864c8a
yashvardhansathe1574 <yashvardhansathe9@gmail.com>
2026-03-30 22:40:17 +0530

    Create substraction.cpp

---
+++ substraction.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a, b, result;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    result = a - b;
+
+    cout << "Subtraction = " << result << endl;
+
+    return 0;
+}
