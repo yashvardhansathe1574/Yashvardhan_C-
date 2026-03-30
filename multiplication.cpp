3afe1c08d064a2aec7a2e198d6d55bda42689d1c
yashvardhansathe1574 <yashvardhansathe9@gmail.com>
2026-03-30 22:41:12 +0530

    Create multiplication.cpp

---
+++ multiplication.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a, b, result;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    result = a * b;
+
+    cout << "Multiplication = " << result << endl;
+
+    return 0;
+}
