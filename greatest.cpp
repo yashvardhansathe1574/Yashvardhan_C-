8e1c325ab88226ee18a50e948fc381a06a18f1dc
yashvardhansathe1574 <yashvardhansathe9@gmail.com>
2026-03-30 22:32:39 +0530

    Create greatest.cpp
    tis is te greatest in three numbers program

---
+++ greatest.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+int main(){
+int a,b,c;
+cout<<"Enter three number:";
+cin>>a>>b>>c>>;
+if(a>=b && a>+c){
+cout<<"greatest number is :"<<a;
+else if(b>=a&&b>=c){
+cout<<"greatest number is:"<<b;
+}
+else{
+cout<<"greatest number is :"<<c;
+}
+return 0;
+}
